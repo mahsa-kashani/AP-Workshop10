e77b5adcdc9f333f2a33ab3d15f775ff824b55ec
Mohammad matin <mahsa12.kashani@gmail.com>
2023-05-29 14:49:42 +0330

    Created ServerSocket and started listening

---
+++ Server/src/Server.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(6000);
+            System.out.println("Server is Listening...");
+            while(true){
+                Socket client = serverSocket.accept();
+                
+            }
+        } catch (IOException e) {
+            System.out.println("The port is occupied");
+        }
+
+    }
+}
