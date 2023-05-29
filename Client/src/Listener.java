893a4ff024bbd7b0d167ccbbcf393d86b3bf3491
Mohammad matin <mahsa12.kashani@gmail.com>
2023-05-29 15:13:13 +0330

    Update Listener.java

--- Client/src/Listener.java
+++ Client/src/Listener.java
@@ -1,4 +1,11 @@
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class Listener implements Runnable{
+    Socket socket;
+    ObjectInputStream oin;
+
     @Override
     public void run() {
 
